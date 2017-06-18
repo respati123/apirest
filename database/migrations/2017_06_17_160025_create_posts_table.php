4f96328d534d0a1dd577efb526bf97e5a5894c8b
= <=>
2017-06-19 03:44:17 +0700

    migrate

--- database/migrations/2017_06_17_160025_create_posts_table.php
+++ database/migrations/2017_06_17_160025_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            //$table->integer('user_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->string('content');
             $table->timestamps();
         });
